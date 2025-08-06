ca01d41e44780582e9580a252edc72a437bc6880
Jhon <jhonalvertmarbebe0@gmail.com>
2025-08-06 10:06:36 +0800

    "data binding and making a dynamic form"

--- AngularBootstrap/src/app/components/home/home.ts
+++ AngularBootstrap/src/app/components/home/home.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
 
+
+  components: string = " ";
+  width: string = " ";
+  word: string = " ";
+  redColor: string = " ";  
+ hide: boolean = true;
+
+  showText(){
+ 
+    this.word = "Alvert";
+    this.redColor = "text-danger";
+    this.hide = false;
+  }
+
+   name!:any;
+
+  changeAlert(){
+
+    alert(this.name);
+  }
+
+
+
+
+
+  firstname!: string;
+  middlename!: string;
+  lastname!: string;
+  gender!: string;
+  course!: string;
+  fullname!:string;
+  courses: string = "N/A";
+  genders: string = "N/A";
+
+  submitForm(){
+
+   if(this.firstname==undefined || this.middlename == undefined || this.lastname==undefined){
+
+     alert("Please fill first");
+     
+   }
+   else{
+
+      this.fullname = this.firstname + ' ' + this.middlename + ' ' + this.lastname;
+      this.courses = this.course;
+      this.genders = this.gender;
+   }
+
+  
+    
+
+  }
 }
